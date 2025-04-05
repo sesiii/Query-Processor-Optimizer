cb5ff88a94f031f60f6179f79b2ca5d6f9f2bdea
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-04-05 12:59:04 +0530

    Removed alias from salary column (2025-04-05 12:59:01)

--- code/query.sql
+++ code/query.sql
@@ -1,1 +1,1 @@
-SELECT t1, t2 FROM employees JOIN salaries ON t1.emp_id = t2.emp_id WHERE t2.salary > 50000;
+SELECT t1, t2 FROM employees JOIN salaries ON t1.emp_id = t2.emp_id WHERE salary > 50000;
