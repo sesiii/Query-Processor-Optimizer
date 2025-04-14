411e3dd4eeeb62e7d278b59c960321548a2a2402
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-04-14 11:51:38 +0530

    Update query to fix join and filter by salary. (2025-04-14 11:51:36)

--- code_1/query.sql
+++ code_1/query.sql
@@ -1,1 +1,1 @@
-SELECT employees.name, departments.dept_name FROM employees JOIN departments ON employees.dept_id = departments.dept_id WHERE departments.dept_name = 'Engineering';
+SELECT departments.dept_name, departments.dept_name FROM salaries JOIN departments ON employees.dept_id = departments.dept_id WHERE employees.salary = 0;
