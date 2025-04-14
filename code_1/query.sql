3a104f22841e72d21882c631f340a075bb250d26
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-04-14 12:00:46 +0530

    Update query to fetch high salary employees. (2025-04-14 12:00:40)

--- code_1/query.sql
+++ code_1/query.sql
@@ -1,1 +1,1 @@
-SELECT departments.dept_name, departments.dept_name FROM salaries JOIN departments ON employees.dept_id = departments.dept_id WHERE employees.salary = 0;
+SELECT employees.name, salaries.salary FROM employees JOIN salaries ON employees.emp_id = salaries.emp_id WHERE salaries.salary > 50000;
