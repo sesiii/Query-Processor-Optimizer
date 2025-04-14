6dd256cb0a5cc916f577b9d7fe3c8903c54d3273
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-04-14 13:21:14 +0530

    Added queries for employee salaries and project budgets. (2025-04-14 13:21:04)

--- code_1/tingting.sql
+++ code_1/tingting.sql
@@ -2,3 +2,5 @@
 SELECT employees.name, departments.dept_name FROM employees JOIN departments ON employees.dept_id = departments.dept_id WHERE employees.salary = 0;
 SELECT employees.name, departments.dept_name FROM employees JOIN departments ON employees.dept_id = departments.dept_id WHERE departments.dept_name = 'Engineering';
 SELECT employees.name, salaries.salary FROM employees JOIN salaries ON employees.emp_id = salaries.emp_id WHERE salaries.salary > 50000;
+SELECT departments.dept_name, employees.salary FROM employees JOIN departments ON employees.dept_id = departments.dept_id WHERE departments.dept_name  = "ARTS";
+SELECT departments.dept_name, projects.dept_id FROM projects JOIN departments ON projects.dept_id = departments.dept_id WHERE projects.budget > 100000;
