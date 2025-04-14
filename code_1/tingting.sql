7a3e8084a8aad0aade41bc0cc2a34b2102588219
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-04-14 11:51:47 +0530

    Added new query, removed newline, fixed salary join. (2025-04-14 11:51:42)

--- code_1/tingting.sql
+++ code_1/tingting.sql
@@ -1,2 +1,3 @@
 SELECT employees.name, departments.dept_name FROM employees JOIN departments ON employees.dept_id = departments.dept_id WHERE departments.dept_name = 'Engineering';
 SELECT employees.name, salaries.salary FROM employees JOIN salaries ON employees.emp_id = salaries.emp_id WHERE salaries.salary > 50000;
+SELECT departments.dept_name, departments.dept_name FROM salaries JOIN departments WHERE employees.salary = 0;
