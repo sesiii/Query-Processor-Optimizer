b9af0073f5c8d846c8d4653f997a99d881ec3eb6
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-04-15 20:10:56 +0530

    "Remove comments" (2025-04-15 20:10:55)

--- code/optimizer.hpp
+++ code/optimizer.hpp
@@ -20,10 +20,10 @@ typedef struct CostMetrics {
 } CostMetrics;
 
 
-// Main function to optimize a query plan
+
 Node* optimize_query(Node *root);
 
-// Helper functions
+
 Node* push_down_selections(Node *node);
 Node* push_down_projections(Node *node);
 Node* reorder_joins(Node *node);
@@ -31,7 +31,7 @@ Node* reorder_joins(Node *node);
 JoinOrderNode* find_optimal_join_order(Node *join_node);
 CostMetrics estimate_cost(Node *node);
 
-// Print the execution plan
+
 void print_execution_plan(Node *node, const char *title);
 
 #endif
