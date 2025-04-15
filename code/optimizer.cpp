408c62370f6fb46a58590baf018f39a0fa29b327
Dadi Sasank Kumar <sesidadi1103@kgpian.iitkgp.ac.in>
2025-04-15 20:10:51 +0530

    "Enable join reordering" (2025-04-15 20:10:47)

--- code/optimizer.cpp
+++ code/optimizer.cpp
@@ -9,7 +9,7 @@
 // Flag to enable/disable optimizations
 int enable_selection_pushdown = 1;
 int enable_projection_pushdown = 1;
-int enable_join_reordering = 0;
+
 int debugkaru = 0;
 
 Node* duplicate_node(Node *node) {
